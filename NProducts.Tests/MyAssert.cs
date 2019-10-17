8cce8d912ff206d252edbf28dab13317b0b0f6cd
nivanovknet <nivanov.knet@gmail.com>
2019-10-17 09:44:56 +0400

    build fix

---
+++ NProducts.Tests/MyAssert.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace NProducts.Tests.Extentions
+{
+    public static class MyAssert
+    {
+        public static T IsType<T>(Object obj)
+        {
+            Assert.IsInstanceOfType(obj, typeof(T));
+            return (T)obj;
+        }
+    }
+}
